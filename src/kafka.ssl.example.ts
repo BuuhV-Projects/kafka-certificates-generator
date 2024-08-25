01f0d543d21b64a3870762db1326adb150aaa1c7
Bruno <info@buuhv.com>
2024-08-25 13:51:40 -0300

    feat: Suporte ao Schema Registry

--- src/kafka.ssl.example.ts
+++ src/kafka.ssl.example.ts
@@ -1,8 +1,10 @@
 require('dotenv').config();
-import { Kafka, Partitioners } from 'kafkajs';
+import { SchemaRegistry } from '@kafkajs/confluent-schema-registry';
 import fs from 'fs';
+import { Kafka, Partitioners } from 'kafkajs';
 import path from 'path';
-const brokers = process.env.HOSTS.split(',').map((host, key) => `${host}:2909${key+2}`);
+
+const brokers = process.env.KAFKA_JS_HOSTS.split(',');
 
 // Carregando os certificados e as chaves necessárias
 const kafka = new Kafka({
@@ -16,8 +18,13 @@ const kafka = new Kafka({
     },
 });
 
-const producer = kafka.producer(({ createPartitioner: Partitioners.DefaultPartitioner }));
+const producer = kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner });
 const consumer = kafka.consumer({ groupId: 'test-group' });
+
+const registry = new SchemaRegistry({ host: 'http://localhost:8081', auth: {
+    password: process.env.KAFKA_PASSWORD,
+    username: process.env.KAFKA_USERNAME,
+},  });
 
 const run = async () => {
     const admin = kafka.admin();
@@ -27,7 +34,7 @@ const run = async () => {
 
     // Criando um tópico
     await admin.createTopics({
-        topics: [{ topic: 'test-topic', numPartitions: 5, replicationFactor: 2, }],
+        topics: [{ topic: 'test-topic', numPartitions: 5, replicationFactor: 2 }],
         waitForLeaders: true,
     });
 
@@ -40,20 +47,25 @@ const run = async () => {
     await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
 
     await consumer.run({
-        eachMessage: async ({ topic, partition, message }) => {
+        eachMessage: async ({ topic: _, partition, message }) => {
+            const decodedValue = await registry.decode(message.value);
             console.log({
                 partition,
                 offset: message.offset,
-                value: message.value?.toString(),
+                value: decodedValue,
             });
         },
     });
 
+    // Obtendo o ID 1 do esquema
+    const schemaId = 1;
     // Produzir uma mensagem
+    const encodedValue = await registry.encode(schemaId, { message: 'Hello KafkaJS user!' });
+
     await producer.send({
         topic: 'test-topic',
         messages: [
-            { value: 'Hello KafkaJS user!' },
+            { value: encodedValue },
         ],
     });
 };
